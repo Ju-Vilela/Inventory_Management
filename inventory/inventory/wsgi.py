2c7a6715fc3e97588995c40f603fec311a022035
Julya Vilela <julyasoaresvilela@gmail.com>
2025-04-02 23:32:41 -0300

    Corrigido WSGI para Vercel

--- inventory/inventory/wsgi.py
+++ inventory/inventory/wsgi.py
@@ -1,11 +1,9 @@
 import os
-
 from django.core.wsgi import get_wsgi_application
-from inventory.wsgi import application
 
-# O Vercel precisa de um "handler" para rodar a aplicação
-app = application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventory.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inventory.settings")
 
 application = get_wsgi_application()
+
+# Adiciona um alias para o Vercel reconhecer
+app = application
