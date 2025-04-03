0a89ac5e4b6d5eea1a57cf71a8a9dd47d9588dbb
Julya Vilela <julyasoaresvilela@gmail.com>
2025-04-02 23:35:57 -0300

    ALLOWED_HOSTS

--- inventory/inventory/settings.py
+++ inventory/inventory/settings.py
@@ -13,8 +13,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "change-me")
 # DEBUG
 DEBUG = os.getenv("DEBUG") == "True"
 
-# ALLOWED_HOSTS
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 # DATABASES
 DATABASES = {
