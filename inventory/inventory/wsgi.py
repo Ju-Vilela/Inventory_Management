f182a8880d5aedd1ceac1e767adef050ccf242ae
Julya Vilela <julyasoaresvilela@gmail.com>
2025-04-10 23:37:35 -0300

    wsgi

--- inventory/inventory/wsgi.py
+++ inventory/inventory/wsgi.py
@@ -1,11 +1,9 @@
 import os
-import sys
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'inventory'))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inventory.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventory.settings')
 
 application = get_wsgi_application()
 
 # Adiciona um alias para o Vercel reconhecer
-app = application
+#app = application
