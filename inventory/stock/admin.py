6a2421eadcb5d717367371c15a1f4faea614708c
Julya Vilela <julyasoaresvilela@gmail.com>
2025-04-12 02:00:04 -0300

    admin

--- inventory/stock/admin.py
+++ inventory/stock/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(User, UserAdmin)
+# Personalizando a exibição no admin
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username',)
+
+# Desregistrando o User padrão e registrando com a personalização
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
