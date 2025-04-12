4c4d428fe97767db16f187b9408d069c4f8c2e93
Julya Vilela <julyasoaresvilela@gmail.com>
2025-04-12 12:31:54 -0300

    Profile

--- inventory/stock/models.py
+++ inventory/stock/models.py
@@ -1,58 +1,6 @@
 from django.db import models
 from django.conf import settings
 
-class Produto(models.Model):
-    item = models.CharField(max_length=100)
-    categoria = models.CharField(max_length=100)
-    marca = models.CharField(max_length=100)
-    validade = models.DateField(null=True, blank=True)
-    vendas = models.IntegerField(default=0)
-    estoque = models.IntegerField()
-    preco = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return self.item
-
-from django.db import models
-from django.contrib.auth.models import User
-
-class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    cargo = models.CharField(max_length=30, choices=[
-        ('dono', 'Dono'),
-        ('admin', 'Administrador'),
-        ('funcionario', 'Funcionário')
-    ])
-    pode_cadastrar = models.BooleanField(default=False)
-    pode_editar = models.BooleanField(default=False)
-    pode_excluir = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f"{self.user.username} ({self.cargo})"
-
-# sempre que um usuário novo for criado, o Profile dele também sera automaticamente gerado.
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-@receiver(post_save, sender=User)
-def criar_ou_atualizar_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-    else:
-        instance.profile.save()
-
-# HISTORICO
-from django.contrib.auth.models import User
-
-class LogDeAcao(models.Model):
-    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    acao = models.CharField(max_length=50)
-    descricao = models.TextField()
-    data = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.usuario.username} - {self.acao} - {self.data}"
-
 # CUSTOMUSER
 from django.contrib.auth.models import AbstractUser
 from django.db import models
@@ -85,6 +33,7 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -103,3 +52,36 @@ class Product(models.Model):
         return user.is_admin
 
 
+class Produto(models.Model):
+    item = models.CharField(max_length=100)
+    categoria = models.CharField(max_length=100)
+    marca = models.CharField(max_length=100)
+    validade = models.DateField(null=True, blank=True)
+    vendas = models.IntegerField(default=0)
+    estoque = models.IntegerField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.item
+
+# sempre que um usuário novo for criado, o Profile dele também sera automaticamente gerado.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=User)
+def criar_ou_atualizar_profile(sender, instance, created, **kwargs):
+    profile, created = Profile.objects.get_or_create(user=instance)
+    if not created:
+        profile.save()
+
+# HISTORICO
+from django.contrib.auth.models import User
+
+class LogDeAcao(models.Model):
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    acao = models.CharField(max_length=50)
+    descricao = models.TextField()
+    data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.usuario.username} - {self.acao} - {self.data}"
